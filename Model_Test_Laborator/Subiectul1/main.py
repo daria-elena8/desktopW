c56923f2635e2fe850e1676bf8dc197892667634
Dariut <daria-elena.tache@s.unibuc.ro>
2023-01-12 17:43:35 +0200

    Subiectul 1 python ok

--- Model_Test_Laborator/Subiectul1/main.py
+++ Model_Test_Laborator/Subiectul1/main.py
@@ -1,14 +1,51 @@
 doc=[]
+x=[]
 l=[]
 def citire_numere(f):
     line=f.readline()
     while line!="":
-       l=line.strip().split(" ")
-       doc.append(l)
-       line=f.readline()
-
+          x=line.strip().split(" ")
+          l=[]
+          for elem in x:
+               l.append(int(elem))
+          doc.append(l)
+          line=f.readline()
     f.close()
+
+def prelucrare_lista(doc):
+    m=len(doc[0])
+    for line in doc:
+        mini = 1000
+        for elem in line:
+            if elem<mini:
+                mini=elem
+        while mini in line:
+            try:
+               line.remove(mini)
+            except ValueError:
+               break
+        if len(line)<m:
+            m=len(line)
+    for x in range(len(doc)):
+        line=[]
+        line=doc[x]
+        line=line[:m]
+        doc[x]=line
 
 f=open("numere.in", "r")
 citire_numere(f)
 print(doc)
+
+d=[]
+k=int(input("k="))
+for line in doc:
+    for elem in line:
+        if k==len(str(elem)):
+            if elem not in d:
+                d.append(elem)
+d.sort(reverse=True)
+print(d)
+
+prelucrare_lista(doc)
+for line in doc:
+    print(*line)
